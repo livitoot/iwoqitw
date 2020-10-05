f5b01bf66922d81d9397de26636e2269c3ae2c12
livitoot <72365527+livitoot@users.noreply.github.com>
2020-10-04 18:08:40 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,6 @@
+basic.forever(function () {
+    music.playMelody("C5 B A G A B C5 C5 ", 120)
+    basic.showIcon(IconNames.Heart)
+    basic.showIcon(IconNames.Asleep)
+    basic.showIcon(IconNames.Skull)
+})
